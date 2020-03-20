1cf2e18faf7b5559b4ab19688db53fd5beae891d
Abr06 <61751911+Abr06@users.noreply.github.com>
2020-03-19 19:09:16 -0700

    Programa de conexion con mysql
    Programa que conecta C# con mysql server, si este se ejecuta muestra las bases de datos creados en la maquina local.
    Es necesario cambiar el usuario y contraseña por uno de la maquina local

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;//Libreria para conexion
+using System;
+
+namespace MySQL_DDL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datos = "";
+            string cadena = "server=localhost; port=3306; user id=Abr06; password=susana24; database=mysql;";//Se crea la cadena para instanciar la base de datos
+            MySqlConnection connection = new MySqlConnection(cadena);
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = null;
+                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES", connection);//instruccion para mostrar bases de datos
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    datos += reader.GetString(0) + "\n";
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine(datos);
+            Console.ReadKey();
+        }
+    }
+}
