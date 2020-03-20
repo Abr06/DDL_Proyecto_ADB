a7b1a811f60b84abf3c4f23cfe9110399e3af53e
Abr06 <61751911+Abr06@users.noreply.github.com>
2020-03-19 21:31:04 -0700

    Creacion de metodos
    Se crearon metodos para facilitar las conecciones y un metodo que permite visualizar todas las bases de datos.

--- Program.cs
+++ Program.cs
@@ -7,26 +7,8 @@ namespace MySQL_DDL
     {
         static void Main(string[] args)
         {
-            string datos = "";
-            string cadena = "server=localhost; port=3306; user id=Abr06; password=susana24; database=mysql;";//Se crea la cadena para instanciar la base de datos
-            MySqlConnection connection = new MySqlConnection(cadena);
-            try
-            {
-                connection.Open();
-                MySqlDataReader reader = null;
-                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES", connection);//instruccion para mostrar bases de datos
-                reader = cmd.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    datos += reader.GetString(0) + "\n";
-                }
-            }
-            catch (MySqlException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            Console.WriteLine(datos);
+            MySQL mySQL = new MySQL("localhost", "3306", "Abr06", "susana24");//puerto default de mysql 3306
+            Console.WriteLine(mySQL.VerBases());
             Console.ReadKey();
         }
     }
